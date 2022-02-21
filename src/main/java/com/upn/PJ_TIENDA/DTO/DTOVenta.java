e9f256c5d62355d5df3bc4946b12ebfc22580317
joselynvcr <n00057871@upn.pe>
2022-02-21 05:27:46 -0500

    _

--- src/main/java/com/upn/PJ_TIENDA/DTO/DTOVenta.java
+++ src/main/java/com/upn/PJ_TIENDA/DTO/DTOVenta.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.upn.PJ_TIENDA.DTO;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author RYZEN7
- */
+
 
 public class DTOVenta {
     @NotBlank(message="Es obligatorio")
